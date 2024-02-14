c786164cd228cd39a8f1f84b3906e06dd915557d
revanth <revanthb2525@gmail.com>
2024-02-14 10:28:52 +0530

    operators

---
+++ Operators.java
@@ -0,0 +1,37 @@
+public class Operators {
+    public static void main(String [] args){
+
+    
+    int a = 5;  
+    int b = 7;  
+    int c = a + b;
+
+    int d = 10;  
+    int e = 3;  
+    int f = d - e;
+
+    int g = 3;  
+    int h = 4;  
+    int i = g * h;
+
+    int j = 10;  
+    int k = 2;  
+    int l = j / k;
+
+    int m = 10;  
+    int n = 3;  
+    int o = m / n;
+
+    int p = 10;  
+    int q = 3;  
+    int r = p % q;
+
+        System.out.println(c);
+        System.out.println(f);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(o);
+        System.out.println(r);
+    }
+}
+
