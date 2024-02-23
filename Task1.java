d30e9e5a3d7e1d1afdcfbac3d6f1d598c65409d7
revanth <revanthb2525@gmail.com>
2024-02-23 18:23:57 +0530

    Modified task output

--- Task1.java
+++ Task1.java
@@ -1,29 +1,30 @@
 import java.util.Scanner;
+
 public class Task1 {
     public static void main(String args[]) {
         getData();
-        while(true) {
+        while (true) {
             Scanner sc = new Scanner(System.in);
             System.out.print("Enter your choice:");
             int a = sc.nextInt();
             System.out.print("Enter any two numbers:");
-            int num1 =  sc.nextInt();
+            int num1 = sc.nextInt();
             int num2 = sc.nextInt();
             switch (a) {
                 case 1:
-                    System.out.println(num1+"+"+num2+"="+(num1+num2));
+                    System.out.println(num1 + "+" + num2 + "=" + (num1 + num2));
                     break;
                 case 2:
-                    System.out.println(num1+"-"+num2+"="+(num1-num2));
+                    System.out.println(num1 + "-" + num2 + "=" + (num1 - num2));
                     break;
                 case 3:
-                    System.out.println(num1+"*"+num2+"="+(num1*num2));
+                    System.out.println(num1 + "*" + num2 + "=" + (num1 * num2));
                     break;
                 case 4:
-                    System.out.println(num1+"/"+num2+"="+(num1/num2));
+                    System.out.println(num1 + "/" + num2 + "=" + (num1 / num2));
                     break;
                 case 5:
-                    System.out.println(num1+"%"+num2+"="+(num1%num2));
+                    System.out.println(num1 + "%" + num2 + "=" + (num1 % num2));
                     break;
                 default:
                     System.out.println("Please enter valid input");
@@ -31,17 +32,21 @@ public class Task1 {
             }
             System.out.print("Do you want to continue? Yes[Y/y]:");
             char ch = sc.next().charAt(0);
-            if(ch!='Y'&& ch!='y') {
+            if (ch != 'Y' && ch != 'y') {
                 break;
             }
         }
     }
+
     static void getData() {
-        System.out.println("S.No "+" \t "+"  Operators");
-        System.out.println(1+"\t\t"+"+");
-        System.out.println(2+"\t\t"+"-");
-        System.out.println(3+"\t\t"+"*");
-        System.out.println(4+"\t\t"+"/");
-        System.out.println(5+"\t\t"+"%");
+        System.out.println("┌───────────┬─────────────┐");
+        System.out.println("│  S.No     │  Operators  │");
+        System.out.println("├───────────┼─────────────┤");
+        System.out.println("│     1     │      +      │");
+        System.out.println("│     2     │      -      │");
+        System.out.println("│     3     │      *      │");
+        System.out.println("│     4     │      /      │");
+        System.out.println("│     5     │      %      │");
+        System.out.println("└───────────┴─────────────┘");
     }
 }
